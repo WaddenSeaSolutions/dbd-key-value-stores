f8e5b118b7717c213ab53ce2272e43e596a6d06d
patrickstolc <patrickstolc@gmail.com>
2025-03-17 10:05:25 +0100

    fix DI for repositories

--- MongoExample/Program.cs
+++ MongoExample/Program.cs
@@ -1,12 +1,24 @@
 using MongoExample.Application.Services;
+using MongoExample.Domain.Interfaces;
+using MongoExample.Infrastructure.Contexts;
 using MongoExample.Infrastructure.Repositories.MongoDB;
 using MongoExample.Infrastructure.Repositories.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Register repositories
-builder.Services.AddScoped<MongoDBPostRepository>();
-builder.Services.AddScoped<RedisPostRepository>();
+builder.Services.AddScoped<IPostRepository, MongoDBPostRepository>(services =>
+{
+    var dbContext = new MongoDbContext("mongodb://localhost:27017", "mongo-example");
+    return new MongoDBPostRepository(dbContext);
+});
+builder.Services.AddScoped<IPostRepository, RedisPostRepository>(
+    services
+        =>
+    {
+        var dbContext = new RedisDbContext("localhost:6379");
+        return new RedisPostRepository(dbContext, TimeSpan.FromHours(1));
+    });
 
 // Register services
 builder.Services.AddScoped<PostService>();
